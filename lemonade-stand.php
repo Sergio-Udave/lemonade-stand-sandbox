56d5ee4b8c07ce6da63af484a4386c6f20caaa41
Sergio-Udave <132074426+Sergio-Udave@users.noreply.github.com>
2024-02-20 08:37:22 -0600

    Update lemonade-stand.php

--- lemonade-stand.php
+++ lemonade-stand.php
@@ -9,7 +9,6 @@
 * Author URI: https://lemonadestand.org
 * License: GPL v2 or later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Github Plugin URI: https://github.com/Sergio-Udave/lemonade-stand-sandbox
 * Update URI: false
 */
 
